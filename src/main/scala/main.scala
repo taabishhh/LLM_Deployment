8962ad21af68ee40f49968c4c40bfd858bc41c85
mmoha110 <mmoha110@uic.edu>
2024-11-12 20:38:20 -0600

    setup project

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,7 +4,7 @@ def main(): Unit = {
   val mp = Map("x"->1)
   mp.toMap.foreach(println)
   if mp.contains("y") then println(mp("y")) else println("no key y")
-  val lst = List("abc,de", "xyz,,kk")
+  val lst = List("hello, world", "xyz,,kk")
   val csv = lst.map {
     str=>str.replace(',', '-')
   }.mkString(",")
