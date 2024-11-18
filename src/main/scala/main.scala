1a7aaef983fe46d7ee51245c87cc495fd30c9b41
mmoha110 <mmoha110@uic.edu>
2024-11-18 16:17:47 -0600

    test commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,14 @@
 
 @main
 def main(): Unit = {
-  val mp = Map("x"->1)
-  mp.toMap.foreach(println)
-  if mp.contains("y") then println(mp("y")) else println("no key y")
-  val lst = List("hello, world", "xyz,,kk")
-  val csv = lst.map {
-    str=>str.replace(',', '-')
-  }.mkString(",")
-  println(csv)
+//  val mp = Map("x"->1)
+//  mp.toMap.foreach(println)
+//  if mp.contains("y") then println(mp("y")) else println("no key y")
+//  val lst = List("hello, world", "xyz,,kk")
+//  val csv = lst.map {
+//    str=>str.replace(',', '-')
+//  }.mkString(",")
+//  println(csv)
+  println("Hello World")
 }
 
